62a2f569a838ec501ba5c772bb6fcd30edb8cc9f
Chatchai Saratakij <CSaratakij@gmail.com>
2019-05-18 22:43:48 +0700

    Finish : version 1.0

---
+++ Assets/Editor/HierachySection.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoadAttribute]
+public static class HierachySection
+{
+    const char PREFIX_SYMBOL = '-';
+    const string PREFIX = "---";
+    const string DEFAULT_LABEL = "Section";
+    const string DEFAULT_FULL_LABEL = "--- Section ---";
+
+    static int totalObject;
+    static List<int> sectionInstanceID;
+
+
+    static HierachySection()
+    {
+        Initialize();
+    }
+
+    static void OnHierachyChanged()
+    {
+        SectionHandler();
+    }
+
+    static void Initialize()
+    {
+        EditorApplication.hierarchyChanged += OnHierachyChanged;
+        sectionInstanceID = new List<int>();
+
+        var allObject = Resources.FindObjectsOfTypeAll<GameObject>();
+        totalObject = allObject.Length;
+
+        foreach (GameObject obj in allObject)
+        {
+            if (obj.name.Contains(PREFIX))
+            {
+                sectionInstanceID.Add(obj.GetInstanceID());
+            }
+        }
+    }
+
+    static void SectionHandler()
+    {
+        var allObject = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        bool isSomeObjectDeleted = (totalObject > allObject.Length);
+        bool isSomeObjectRename = (totalObject == allObject.Length);
+        bool isSomeObjectAddIn = (totalObject < allObject.Length);
+
+        if (isSomeObjectDeleted)
+        {
+            ClearNonExistSectionID();
+        }
+
+        if (isSomeObjectRename)
+        {
+            Rename(Selection.transforms);
+        }
+
+        if (isSomeObjectAddIn)
+        {
+            Debug.Log("Some object add in..");
+            AddSectionID(Selection.transforms);
+        }
+
+        totalObject = allObject.Length;
+        Debug.Log("Total id : " + sectionInstanceID.Count);
+    }
+
+    static void ClearNonExistSectionID()
+    {
+        List<int> removeList = new List<int>();
+
+        foreach (int id in sectionInstanceID)
+        {
+            Object obj = EditorUtility.InstanceIDToObject(id);
+
+            if (obj == null)
+            {
+                removeList.Add(id);
+            }
+        }
+
+        foreach (int id in removeList)
+        {
+            Debug.Log("Delete non-exist id");
+            sectionInstanceID.Remove(id);
+        }
+
+        removeList.Clear();
+    }
+
+    static void Rename(Transform[] targetObjects)
+    {
+        foreach (Transform obj in targetObjects)
+        {
+            Rename(obj);
+        }
+    }
+
+    static void Rename(Transform targetObject)
+    {
+        if (targetObject == null)
+            return;
+
+        int id = targetObject.gameObject.GetInstanceID();
+        bool isSectionInstance = sectionInstanceID.Contains(id);
+
+        GameObject obj = EditorUtility.InstanceIDToObject(id) as GameObject;
+        bool canRename = false;
+
+        if (obj != null)
+        {
+            canRename = !obj.name.Contains(PREFIX);
+        }
+
+        if (isSectionInstance && canRename)
+        {
+            Debug.Log("Can Rename");
+            string objectName = obj.name.Trim(PREFIX_SYMBOL, ' ');
+
+            if (string.IsNullOrEmpty(objectName))
+            {
+                objectName = DEFAULT_LABEL;
+            }
+
+            obj.name = string.Format(PREFIX + " {0} " + PREFIX, objectName);
+        }
+        else
+        {
+            Debug.Log("Can't..");
+        }
+    }
+
+    static void AddSectionID(Transform[] targetObjects)
+    {
+        foreach (Transform obj in targetObjects)
+        {
+            AddSectionID(obj);
+        }
+    }
+
+    static void AddSectionID(Transform targetObject)
+    {
+        if (targetObject == null)
+            return;
+
+        int id = targetObject.gameObject.GetInstanceID();
+
+        bool canAddSectionID = targetObject.gameObject.name.Contains(PREFIX);
+        bool isAlreadyExist = sectionInstanceID.Contains(id);
+
+        if (canAddSectionID && !isAlreadyExist)
+        {
+            sectionInstanceID.Add(targetObject.gameObject.GetInstanceID());
+            Debug.Log("Add id in obj name:" + targetObject.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("Can't add section id of object : " + targetObject.gameObject.name);
+        }
+    }
+
+    [MenuItem("GameObject/Create Section %t", false, 0)]
+    public static void CreateSection()
+    {
+        GameObject obj = new GameObject(DEFAULT_FULL_LABEL);
+
+        obj.transform.position = Vector3.zero;
+        obj.SetActive(false);
+
+        Undo.RegisterCreatedObjectUndo(obj, "Add Section");
+        AddSectionID(obj.transform);
+    }
+}
+
